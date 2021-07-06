f1da2b1b0098209bf4518a7ce9a1b64738d96cc2
oddlyspaced <oddlyspaced1@gmail.com>
2021-07-07 01:12:37 +0530

    RedditData: Setup Data structure

--- app/src/main/java/com/oddlyspaced/redditclone/RedditData.kt
+++ app/src/main/java/com/oddlyspaced/redditclone/RedditData.kt
@@ -1,4 +1,8 @@
 package com.oddlyspaced.redditclone
 
-class RedditData {
-}
+import com.google.gson.annotations.SerializedName
+
+data class RedditData(
+    @SerializedName("children")
+    val children: List<RedditChild>
+)
