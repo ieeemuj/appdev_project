359a8bd8616992a41081f4630b74230144ace7e9
oddlyspaced <oddlyspaced1@gmail.com>
2021-07-07 01:16:13 +0530

    RedditApi: Setup method endpoint for subreddit details

--- app/src/main/java/com/oddlyspaced/redditclone/RedditApi.kt
+++ app/src/main/java/com/oddlyspaced/redditclone/RedditApi.kt
@@ -1,4 +1,12 @@
 package com.oddlyspaced.redditclone
 
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface RedditApi {
+    @GET("{subreddit}.json")
+    fun fetchSubredditData(
+        @Path("subreddit") subredditName: String
+    ): Call<RedditResponse>
 }
