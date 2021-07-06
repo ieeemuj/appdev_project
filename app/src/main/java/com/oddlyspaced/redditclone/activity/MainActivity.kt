bfff73692d74b6dda9b6216cffd3c8508eb98707
oddlyspaced <oddlyspaced1@gmail.com>
2021-07-07 01:54:37 +0530

    MainActivity: Cleanup and setup view binding variable

--- app/src/main/java/com/oddlyspaced/redditclone/activity/MainActivity.kt
+++ app/src/main/java/com/oddlyspaced/redditclone/activity/MainActivity.kt
@@ -6,28 +6,19 @@ import android.util.Log
 import com.oddlyspaced.redditclone.R
 import com.oddlyspaced.redditclone.api.RedditClient
 import com.oddlyspaced.redditclone.api.model.RedditResponse
+import com.oddlyspaced.redditclone.databinding.ActivityMainBinding
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        RedditClient.getApiClient().fetchSubredditData("wallpapers").enqueue(object : Callback<RedditResponse> {
-            override fun onResponse(call: Call<RedditResponse>, response: Response<RedditResponse>) {
-                response.body()?.let {
-                    it.data.children.forEach { child ->
-                        Log.e("Titles", child.data.title)
-                    }
-                }
-            }
-
-            override fun onFailure(call: Call<RedditResponse>, t: Throwable) {
-                t.printStackTrace()
-            }
-
-        })
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
