7142ed55dc5666e57dc60763bc2f7d8414b680d4
oddlyspaced <oddlyspaced1@gmail.com>
2021-07-07 02:17:43 +0530

    MainActivity: Setup code for displaying data

--- app/src/main/java/com/oddlyspaced/redditclone/activity/MainActivity.kt
+++ app/src/main/java/com/oddlyspaced/redditclone/activity/MainActivity.kt
@@ -3,8 +3,11 @@ package com.oddlyspaced.redditclone.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.oddlyspaced.redditclone.R
+import com.oddlyspaced.redditclone.adapter.RedditAdapter
 import com.oddlyspaced.redditclone.api.RedditClient
+import com.oddlyspaced.redditclone.api.model.RedditChildData
 import com.oddlyspaced.redditclone.api.model.RedditResponse
 import com.oddlyspaced.redditclone.databinding.ActivityMainBinding
 import retrofit2.Call
@@ -14,11 +17,55 @@ import retrofit2.Response
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val client = RedditClient.getApiClient()
+
+    private val data = arrayListOf<RedditChildData>()
+    private val adapter = RedditAdapter(data)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        init()
+        initListener()
+    }
+
+    private fun init() {
+        binding.rvReddit.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = this@MainActivity.adapter
+        }
+    }
+
+    private fun initListener() {
+        binding.btnSubreddit.setOnClickListener {
+            val text = binding.etSubredditNameInput.text.toString()
+            if (text.isNotEmpty()) {
+                loadData(text)
+            }
+        }
+    }
+
+    private fun loadData(subreddit: String) {
+        client.fetchSubredditData(subreddit).enqueue(object: Callback<RedditResponse> {
+            override fun onResponse(call: Call<RedditResponse>, response: Response<RedditResponse>) {
+                if (response.isSuccessful) {
+                    response.body()?.let {
+                        data.clear()
+                        it.data.children.forEach { child ->
+                            data.add(child.data)
+                        }
+                        adapter.notifyDataSetChanged()
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<RedditResponse>, t: Throwable) {
+                t.printStackTrace()
+            }
+
+        })
     }
 }
