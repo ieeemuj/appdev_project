f9b1914bf66e398337f6ca4bc0e11702d598c021
oddlyspaced <oddlyspaced1@gmail.com>
2021-07-07 01:25:43 +0530

    RedditClient: Create client object class

--- app/src/main/java/com/oddlyspaced/redditclone/api/RedditClient.kt
+++ app/src/main/java/com/oddlyspaced/redditclone/api/RedditClient.kt
@@ -1,4 +1,23 @@
 package com.oddlyspaced.redditclone.api
 
-class RedditClient {
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+object RedditClient {
+    fun getApiClient(): RedditApi {
+        val gson = GsonBuilder().apply {
+            setLenient()
+        }.create()
+        val okHttpClient = OkHttpClient.Builder().apply {
+            readTimeout(5000, TimeUnit.SECONDS)
+            connectTimeout(100, TimeUnit.SECONDS)
+        }.build()
+        return Retrofit.Builder().apply {
+            baseUrl("https://www.reddit.com/r/").client(okHttpClient)
+            addConverterFactory(GsonConverterFactory.create(gson))
+        }.build().create(RedditApi::class.java)
+    }
 }
