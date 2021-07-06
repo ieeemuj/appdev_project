ddc87242609e19aa5e38d33d59c6e527ca8bc42e
oddlyspaced <oddlyspaced1@gmail.com>
2021-07-07 01:30:02 +0530

    MainActivity: Test wallpaper data fetching

--- app/src/main/java/com/oddlyspaced/redditclone/MainActivity.kt
+++ app/src/main/java/com/oddlyspaced/redditclone/MainActivity.kt
@@ -2,10 +2,31 @@ package com.oddlyspaced.redditclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.oddlyspaced.redditclone.api.RedditClient
+import com.oddlyspaced.redditclone.api.model.RedditResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        RedditClient.getApiClient().fetchSubredditData("wallpapers").enqueue(object : Callback<RedditResponse> {
+            override fun onResponse(call: Call<RedditResponse>, response: Response<RedditResponse>) {
+                response.body()?.let {
+                    it.data.children.forEach { child ->
+                        Log.e("Titles", child.data.title)
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<RedditResponse>, t: Throwable) {
+                t.printStackTrace()
+            }
+
+        })
     }
 }
