3eff7e77c8944f3ffa62c0707fab0195cd461660
oddlyspaced <oddlyspaced1@gmail.com>
2021-07-07 01:47:39 +0530

    RedditAdapter: Setup adapter with view holder

--- app/src/main/java/com/oddlyspaced/redditclone/adapter/RedditAdapter.kt
+++ app/src/main/java/com/oddlyspaced/redditclone/adapter/RedditAdapter.kt
@@ -1,4 +1,30 @@
 package com.oddlyspaced.redditclone.adapter
 
-class RedditAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.oddlyspaced.redditclone.api.model.RedditChildData
+import com.oddlyspaced.redditclone.databinding.ItemPostBinding
+
+class RedditAdapter(private val data: ArrayList<RedditChildData>) : RecyclerView.Adapter<RedditAdapter.RedditViewHolder>() {
+
+    inner class RedditViewHolder(private val binding: ItemPostBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: RedditChildData) {
+            binding.txTitle.text = item.title
+            binding.txAuthor.text = item.author
+            binding.txUpvotes.text = item.upvotes.toString()
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RedditViewHolder {
+        return RedditViewHolder(ItemPostBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun onBindViewHolder(holder: RedditViewHolder, position: Int) {
+        holder.bind(data[position])
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
 }
